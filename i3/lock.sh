c656a3d0509dcff75ffb81f804849a2887c6f791
Willie Jeng <willie.jeng@gmail.com>
2019-01-17 14:48:23 -0800

    Finished install.sh

--- i3/lock.sh
+++ i3/lock.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
-
-scrot /tmp/screen.png
-
-convert /tmp/screen.png -blur 0x8 /tmp/screen.png
-[[ -f ~/.config/i3/lock.png ]] && convert /tmp/screen.png ~/.config/i3/lock.png -gravity center -composite /tmp/screen.png
-i3lock -u -e -i /tmp/screen.png
+/home/wjeng/.config/i3/lock.sh -
--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ DOTFILES_PATH=`cd $(dirname $0) ; pwd -P`
 confirm_action() {
 	# print question
 	echo -n $1 "(y/N) "
-	read -n 1 reply
+	read -n 1
 	printf "\n"
 
 	# return reply
-	[[ $reply =~ ^[Yy]$ ]]
+	[[ $REPLY =~ ^[Yy]$ ]]
 }
 
 install_vimrc() {
@@ -23,13 +23,61 @@ install_vimrc() {
 	ln -sf $DOTFILES_PATH/.vimrc $HOME/.vimrc
 }
 
+# $1 is source directory, $2 is target directory 
+simlink_files() {
 
-echo ***WARNING***
-echo YOUR EXISTING CONFIG FILES WILL BE REPLACED AND WOULD NOT BE ABLE TO BE RECOVERED.
-echo PLEASE MAKE A BACKUP OF YOUR OLD CONFIGS BEFORE RUNNING THIS SCRIPT.
-echo "(YOU WILL BE PROMPTED BEFORE EACH PROGRAM'S CONFIGS)"
+	cd $1
 
-confirm_action "Are you sure you want to continue?"
-echo 
+	files=$(find | sed 's/^\.//g' | sed 's/^\///g')
 
-install_vimrc
+	for f in $files; do
+		if [ -e $2/$f ] || [ -L $2/$f ] ; then
+			if confirm_action "$2/$f already exists. Replace?" ; then
+				ln -sf $1/$f $2/$f
+				echo "$2/$f -> $1/$f"
+			fi
+		else
+			ln -s $1/$f $2/$f
+			echo "$2/$f -> $1/$f"
+		fi
+	done
+
+	echo
+}
+
+
+if confirm_action "Install i3 configs?" ; then
+	simlink_files "$DOTFILES_PATH/i3" "$HOME/.config/i3"
+fi
+
+if confirm_action "Install vim configs?" ; then
+	simlink_files "$DOTFILES_PATH/vim" "$HOME"
+fi
+
+if confirm_action "Install zsh configs?" ; then
+	simlink_files "$DOTFILES_PATH/zsh" "$HOME"
+fi
+
+if confirm_action "Install termite configs?" ; then
+	simlink_files "$DOTFILES_PATH/termite" "$HOME/.config/termite"
+fi
+
+if confirm_action "Install ranger configs?" ; then
+	simlink_files "$DOTFILES_PATH/ranger" "$HOME/.config/ranger"
+fi
+
+if confirm_action "Install neofetch configs?" ; then
+	simlink_files "$DOTFILES_PATH/neofetch" "$HOME/.config/neofetch"
+fi
+
+if confirm_action "Install dunst configs?" ; then
+	simlink_files "$DOTFILES_PATH/dunst" "$HOME/.config/dunst"
+fi
+
+if confirm_action "Install rofi configs?" ; then
+	simlink_files "$DOTFILES_PATH/rofi" "$HOME/.config/rofi"
+fi
+
+if confirm_action "Install cava configs?" ; then
+	simlink_files "$DOTFILES_PATH/cava" "$HOME/.config/cava"
+fi
